c490d552a4cf1cdd0163865f902bfa7fd360ec2e
Disha Shivhare <dishashivhare44@gmail.com>
2025-03-18 23:09:55 +0530

    Navbar All button Completed

--- src/Components/NavBar/CategorySelector.jsx
+++ src/Components/NavBar/CategorySelector.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 
 function CategorySelector() {
+  let Categories=["All","Scalves","Hats","Jackets","Socks"]
+  let title="All"
   return (
-    <h2>
-      category selector
-    </h2>
+    <div className='dropdown mb-3 mb-lg-0'>
+      <button className='btn btn-outline-success text-white dropdown-toggle'type='button' id='dropDownButton1' data-bs-toggle='dropdown'>{title}</button>
+      <ul className='dropdown-menu'>
+        {
+            Categories.map((Category)=>{
+              return <li key={Category}><a href="#" className='dropdown-item pointer'>{Category}</a></li>
+            })
+        }
+      </ul>
+    </div>
   )
 }
 
--- src/Components/NavBar/Nav.jsx
+++ src/Components/NavBar/Nav.jsx
@@ -5,17 +5,16 @@ import CartButton from './CartButton'
 
 function Nav() {
   return (
-    <nav className='navbar navbar-eaxpand-lg navbar-dark bg-dark border-bottom fixed-top'>
+    <nav className='navbar navbar-expand-lg navbar-dark bg-dark border-bottom fixed-top'>
         <div className='container-fluid px-md-5'>
           <span id='name' className='navbar-brand fw-bold pointer'>Ezee-Cart-Blitz</span>
-          <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bf-target='#navbarSupportedContent'>
-            <span className='navbar-toggler-icon'></span>
+          <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarSupportedContent'> 
+             <span className='navbar-toggler-icon'></span>
           </button>
-          <div className='collapse navbar-collapse justify-contetnt-end' id='navbarSupportedContent'>
-            Heelo
-            <CategorySelector/>
-            <SearchBar/>
-            <CartButton/>
+          <div className='collapse navbar-collapse justify-content-end' id='navbarSupportedContent'>
+              <CategorySelector/>
+            {/* <SearchBar/>
+            <CartButton/> */}
             </div>
         </div>
     </nav>
