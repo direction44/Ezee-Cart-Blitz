b004c559ff189a15f4af364bb74f19b4e04e723b
Disha Shivhare <dishashivhare44@gmail.com>
2025-03-22 23:44:49 +0530

    render indivisual component

--- src/Components/Pages/Single.jsx
+++ src/Components/Pages/Single.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-
+import { useParams } from 'react-router'
+import data from '../../data'
 function Single() {
+  const {id}=useParams()
+    const product=data.find((product)=>product.id=== +id)
+    const imagesPath="/images/"+product.id+".jpg"
+    console.log(imagesPath,"**************")
     return (
-        <h1>
-          Single
-        </h1>
+      <div className='row'>
+        <div className='col-6'>
+          <img src={imagesPath} alt="" className='w-50' />
+        </div>
+        <div className='col-6'>
+          <h2>{product.name}</h2>
+          <h2>{product.price}</h2>
+        </div>
+      </div>
+        
       )
 }
 
