80a6d46ac3d427db11670a0c1ecb6a63b2c76322
Disha Shivhare <dishashivhare44@gmail.com>
2025-03-22 23:37:33 +0530

    Added Pages folder with all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import Nav from './Components/NavBar/Nav'
 import {Routes,Route} from "react-router-dom"
+import Home from './Components/Pages/Home'
+import Single from './Components/Pages/Single'
+import Cart from './Components/Pages/Cart'
+
 function App() {
   return (
     <div className='wrapper bg-dark text-white'>
       <Nav title="Ezee-Cart-Blitz"/>
       <div className='container mt-5 py-5 px-3 px-md-5'>
         <Routes>
-          <Route path="/" element={<h1>Home Routes</h1>}></Route>
-          <Route path="/Single/:id" element={<h1>Single Routes</h1>}></Route>
-          <Route path="/Cart"element={<h1>Cart Routes</h1>}></Route >
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/Single/:id" element={<Single/>}></Route>
+          <Route path="/Cart"element={<Cart/>}></Route >
         </Routes>
       </div>
     </div>
---
+++ src/Components/Pages/Single.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Single() {
+    return (
+        <h1>
+          Single
+        </h1>
+      )
+}
+
+export default Single
