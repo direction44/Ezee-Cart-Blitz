0bc653dbe22523da1ddb5a36447482c9a29033a7
Disha Shivhare <dishashivhare44@gmail.com>
2025-03-26 10:24:48 +0530

    Generate ProductButton and added Add To Cart button in all Cards

--- src/Components/Products/Product.jsx
+++ src/Components/Products/Product.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import ProductButton from './ProductButton'
 function Product(props) {
   const {product}=props
   const imagePath="/images/"+product.id+".jpg"
@@ -11,20 +12,19 @@ function Product(props) {
   return (
     <div className='col'>
       <div className='card h-100' id="product">
-        <img src={imagePath} title={product.name} alt="" className='card-img-top pointer'/>
+        <img src={imagePath} title={product.name} alt="" className='card-img-top pointer' onClick={handleClick}/>
         <div className='card-body p-4'>
           <div className='text-center'>
             <h6 className='fw-bolder'>{product.name}</h6>
             <span>{product.price}</span>
           </div>
         </div>
-        <div className='card-footer p-4 pt-4 border-top-0 bg-transparent'>Button here!</div>
+        <div className='card-footer p-4 pt-4 border-top-0 bg-transparent'>
+          <ProductButton/>
+        </div>
       </div>
     </div>
-    // <li onClick={handleClick}>
-    //   {product.name}
-    // </li>
-  )
+      )
 }
 
 export default Product
---
+++ src/Components/Products/ProductButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ProductButton() {
+  return (
+   <button className='btn btn-outline-success d-block w-100'>Add To Cart</button>
+  )
+}
+
+export default ProductButton
