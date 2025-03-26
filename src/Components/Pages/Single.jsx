8b75d3c90f8353b285c4eb198616dddab1c6f7f6
Disha Shivhare <dishashivhare44@gmail.com>
2025-03-26 11:08:16 +0530

    completed UI of each single product with details

--- src/Components/Pages/Single.jsx
+++ src/Components/Pages/Single.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import { useParams } from 'react-router'
 import data from '../../data'
+import ProductButton from '../Products/ProductButton'
 function Single() {
   const {id}=useParams()
     const product=data.find((product)=>product.id=== +id)
     const imagesPath="/images/"+product.id+".jpg"
     return (
-      <div className='row'>
-        <div className='col-6'>
-          <img src={imagesPath} alt="" className='w-50' />
+      <div id='single' className='row justify-content-center align-items-center text-white mx-auto'>
+        <div className='col-md-6'>
+          <img src={imagesPath} alt={product.name} className='card-img-top mb-5 mb-md-0 p-0 p-lg-5'/>
         </div>
-        <div className='col-6'>
-          <h2>{product.name}</h2>
-          <h2>{product.price}</h2>
+        <div className='col-md-6 text-center text-md-start'>
+          <h2 className='fs-1 fw-bold'>{product.name}</h2>
+          <div className='fs-5 mb-2'>{product.price}</div>
+          <p className='lead'>
+            {product.description}
+          </p>
+          <ProductButton/>
         </div>
-      </div>
-        
+      </div>        
       )
 }
 
