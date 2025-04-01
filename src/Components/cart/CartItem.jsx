03cdf2d9a655add5d11ac2506f54152ae8471d58
Disha Shivhare <dishashivhare44@gmail.com>
2025-04-01 14:35:49 +0530

    added functionality of increase/decrease quantity of product in Cart

--- src/Components/cart/CartItem.jsx
+++ src/Components/cart/CartItem.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
-
+import { useDispatch } from 'react-redux'
+import { setQuantity } from '../../features/cart/cartSlice'
 function CartItem(props) {
     const {item}=props
+    const dispatch=useDispatch()
+    const handleClick=(qty)=>{
+        dispatch(setQuantity({item,qty}))
+    }
   return (
    <li className='list-group-item'>
     <div className='my-0 d-flex justify-content-between align-items-center'>
         <span className='fw-bolder fs-6 me-auto'>{item.name}({item.price})</span>
         <div className='btn-group'>
-            <button className='btn border'>-</button>
-            <button className='btn border' disabled>1</button>
-            <button className='btn border'>+</button>
+            <button onClick={()=>{
+                handleClick(-1)
+            }} 
+            className='btn border'>-</button>
+            <button className='btn border' disabled>{item.quantity}</button>
+            <button onClick={()=>{
+                handleClick(1)
+            }}
+            className='btn border'>+</button>
         </div>
     </div>
     <p className='text-muted mb-0 col-3 w-100 description'>{item.description}</p>
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -18,10 +18,18 @@ const cartSlice=createSlice({
             // state.cartItems=state.cartItems.filter((p)=>p.id!=item.id)
             let index=state.cartItems.findIndex((product)=>product.id===item.id) 
             state.cartItems.splice(index,1)        
+        },
+        setQuantity:(state,action)=>{
+            const {item,qty}=action.payload
+            state.cartItems=state.cartItems.map((cartItem)=>{
+                return cartItem.id===item.id?{...cartItem,quantity:cartItem.quantity+qty}:cartItem
+            })
+
+            state.cartItems=state.cartItems.filter((cartItem)=>cartItem.quantity>=1)
         }
     }
 })
 
-export const{addToCart,removeFromCart}=cartSlice.actions
+export const{addToCart,removeFromCart,setQuantity}=cartSlice.actions
 
 export default cartSlice.reducer
